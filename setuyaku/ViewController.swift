b70cb41bc28345520330ec3b9bcfc65fd3d91964
畑川健勝 <kensho8617@gmail.com>
2018-02-15 18:26:35 +0900

    Initial Commit

---
+++ setuyaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  setuyaku
+//
+//  Created by keniku_macbook on 2018/02/15.
+//  Copyright © 2018年 keniku_macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
